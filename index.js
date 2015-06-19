a80cc271fd13072fedce378515fc5665d85a0ada
Rasmus Eskola <fruitiex@gmail.com>
2015-06-19 18:21:28 +0300

    update

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ var Spawn = require('node-spawn');
 var _ = require('underscore');
 var socket = require('socket.io-client')('http://fruitiex.org:9191');
 
+// maximum value observed in a while
+var ival = 0;
+var imaxVal = 0;
+//var minVal = 100;
+var dVal = 0.995;
+
 var audioBuffer = new Buffer(0);
 var windowSize = 4096;
-var avgFactor = 0.9;
+var avgFactor = 0.8;
 var fft = require('kissfft').fft;
 
 var hue = 0;
 var samplingRate = 44100;
-var numBands = 1024;
+var numBands = 256;
+var minF = 0;
+var maxF = 22050;
 var emitBand = 0;
 var avg = Array.apply(null, new Array(windowSize / 2)).map(Number.prototype.valueOf, 0);
 
@@ -27,18 +35,30 @@ var getAmplitude = function(f, spreadF) {
 var avgResult = function(output) {
     _.each(output, function(band, index) {
         if (band) {
-            avg[index] = avg[index] * avgFactor + band * (1 - avgFactor);
+            //avg[index] = avg[index] * avgFactor + band * (1 - avgFactor);
+            avg[index] = band;
         }
     });
 };
 var printSpectrum = function(output) {
     var bands = Array.apply(null, new Array(numBands)).map(Number.prototype.valueOf, 0);
 
+    /*
     _.each(avg, function(band, index) {
         if (band) {
             bands[Math.round((index / (windowSize / 2)) * numBands)] += band;
         }
     });
+    */
+    for (var i = 0; i < numBands; i++) {
+        bands[i] = getAmplitude(minF + (i / numBands) * (maxF - minF), (maxF - minF) / numBands);
+    }
+    /*
+    _.each(bands, function(band, index) {
+        var df = samplingRate / windowSize / 2;
+        band = getAmplitude(df * (index / 
+    });
+    */
 
     /*
     process.stdout.write('\u001B[2J\u001B[0;0f');
@@ -52,13 +72,22 @@ var printSpectrum = function(output) {
     */
 
     // cycle hue by approx. snare drum frequencies
-    hue += getAmplitude(1200, 300) / 100;
-    hue = hue % 256;
+    hue += getAmplitude(1200, 100) / 64;
+    hue = hue % 360;
 
     // adjust intensity by approx. kick drum frequencies
-    var intensity = getAmplitude(100, 50) / 4;
+    var intensity = getAmplitude(20, 50);
+    imaxVal = Math.max(1, Math.max(imaxVal * dVal, intensity));
+    //var ival = Math.max(0, Math.round((intensity * 2 - imaxVal) / (imaxVal) * 50));
+    ival = ival * avgFactor + (1 - avgFactor) * (Math.exp(Math.pow(intensity / imaxVal, 2)) - 1) / (Math.E - 1) * 50;
+    //minVal = Math.min(minVal * (2 - dVal), intensity);
+    console.log();
+    //console.log(minVal);
 
-    socket.emit('color', 'hsl(' + Math.round(hue) + ', 100%, ' + Math.round(intensity) + '%)');
+    console.log(ival);
+    console.log(intensity);
+    //socket.emit('color', 'hsl(' + Math.round(hue) + ', 100%, ' + Math.round(((intensity - minVal) / (imaxVal - minVal)) * 50) + '%)');
+    socket.emit('color', 'hsl(' + Math.round(hue) + ', 100%, ' + Math.round(ival * 1000) / 1000 + '%)');
 };
 
 var runFFT = function(newData) {
