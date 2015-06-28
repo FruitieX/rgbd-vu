5db30fd6d732ce2702bd97d66f400046c2feff44
Rasmus Eskola <fruitiex@gmail.com>
2015-06-29 01:53:44 +0300

    party

--- index.js
+++ index.js
@@ -2,92 +2,76 @@ var fs = require('fs');
 var Spawn = require('node-spawn');
 var _ = require('underscore');
 var socket = require('socket.io-client')('http://fruitiex.org:9191');
+var one = require('onecolor');
 
-// maximum value observed in a while
+// maximum values observed in a while
+var bands = {
+    bass: {
+        bin: 4,
+        value: 0,
+        peak: 0
+    },
+    snare: {
+        bin: 15,
+        value: 0,
+        peak: 0
+    },
+    hihat: {
+        bin: 900,
+        value: 0,
+        peak: 0
+    }
+}
 var ival = 0;
-var imaxVal = 0;
 //var minVal = 100;
-var dVal = 0.995;
+var dPeak = 0.995;
 
 var audioBuffer = new Buffer(0);
 var windowSize = 4096;
-var avgFactor = 0.8;
+var avgFactor = 0.80;
 var fft = require('kissfft').fft;
 
 var hue = 0;
 var samplingRate = 44100;
-var numBands = 256;
 var minF = 0;
 var maxF = 22050;
 var emitBand = 0;
 var avg = Array.apply(null, new Array(windowSize / 2)).map(Number.prototype.valueOf, 0);
 
-var getAmplitude = function(f, spreadF) {
-    var midIndex = Math.round(f * windowSize / samplingRate);
-    var spread = Math.round(spreadF * windowSize / samplingRate);
-
-    var sum = 0;
-    for (var i = Math.max(0, midIndex - spread); i < Math.min(windowSize / 2, midIndex + spread); i++) {
-        sum += avg[i] * Math.exp(1 / (Math.abs(i - midIndex) + 1)) / Math.E;
-    }
-    return sum;
-};
-var avgResult = function(output) {
-    _.each(output, function(band, index) {
-        if (band) {
-            //avg[index] = avg[index] * avgFactor + band * (1 - avgFactor);
-            avg[index] = band;
-        }
-    });
-};
 var printSpectrum = function(output) {
-    var bands = Array.apply(null, new Array(numBands)).map(Number.prototype.valueOf, 0);
-
-    /*
-    _.each(avg, function(band, index) {
-        if (band) {
-            bands[Math.round((index / (windowSize / 2)) * numBands)] += band;
-        }
-    });
-    */
-    for (var i = 0; i < numBands; i++) {
-        bands[i] = getAmplitude(minF + (i / numBands) * (maxF - minF), (maxF - minF) / numBands);
-    }
-    /*
-    _.each(bands, function(band, index) {
-        var df = samplingRate / windowSize / 2;
-        band = getAmplitude(df * (index / 
-    });
-    */
-
-    /*
-    process.stdout.write('\u001B[2J\u001B[0;0f');
-    _.each(bands, function(band, index) {
-        if (index > 32) {
-            return;
-        }
-        var floored = Math.max(0, Math.floor(band * numBands / windowSize));
-        console.log(floored ? Array(floored).join('=') : '');
-    });
-    */
-
-    // cycle hue by approx. snare drum frequencies
-    hue += getAmplitude(1200, 100) / 64;
-    hue = hue % 360;
+    hue += 0.0005;
 
     // adjust intensity by approx. kick drum frequencies
-    var intensity = getAmplitude(20, 50);
-    imaxVal = Math.max(1, Math.max(imaxVal * dVal, intensity));
-    //var ival = Math.max(0, Math.round((intensity * 2 - imaxVal) / (imaxVal) * 50));
-    ival = ival * avgFactor + (1 - avgFactor) * (Math.exp(Math.pow(intensity / imaxVal, 2)) - 1) / (Math.E - 1) * 50;
+    //var intensity = getAmplitude(100, 6);
+    var intensity = output[bands.bass.bin];
+    bands.bass.peak = Math.max(1, Math.max(bands.bass.peak * dPeak, intensity));
+    bands.bass.value = bands.bass.value * avgFactor + (1 - avgFactor) * (Math.exp(Math.pow(intensity / bands.bass.peak, 4)) - 1) / (Math.E - 1);
+    bands.bass.value = Math.max(bands.bass.value, (Math.exp(Math.pow(intensity / bands.bass.peak, 4)) - 1) / (Math.E - 1));
+    //bands.bass.value = (Math.exp(Math.pow(intensity / bands.bass.peak, 2)) - 1) / (Math.E - 1);
     //minVal = Math.min(minVal * (2 - dVal), intensity);
-    console.log();
     //console.log(minVal);
+    //
+    intensity = output[bands.snare.bin];
+    bands.snare.peak = Math.max(1, Math.max(bands.snare.peak * dPeak, intensity));
+    bands.snare.value = bands.snare.value * avgFactor + (1 - avgFactor) * (Math.exp(Math.pow(intensity / bands.snare.peak, 4)) - 1) / (Math.E - 1);
+    bands.snare.value = Math.max(bands.snare.value, (Math.exp(Math.pow(intensity / bands.snare.peak, 4)) - 1) / (Math.E - 1));
 
-    console.log(ival);
-    console.log(intensity);
+    intensity = output[bands.hihat.bin];
+    bands.hihat.peak = Math.max(0.01, Math.max(bands.hihat.peak * dPeak, intensity));
+    bands.hihat.value = bands.hihat.value * avgFactor + (1 - avgFactor) * (Math.exp(Math.pow(intensity / bands.hihat.peak, 4)) - 1) / (Math.E - 1);
+    //bands.hihat.value = Math.max(bands.hihat.value, (Math.exp(Math.pow(intensity / bands.hihat.peak, 4)) - 1) / (Math.E - 1));
+
+    //bands.bass.value = Math.max(bands.bass.value * avgFactor, bands.bass.value * 100 - 25);
+    var bass = bands.bass.value
+    console.log(Math.floor(bands.bass.value * 100) + '\t' + Math.floor(bands.snare.value * 100) + '\t' + Math.floor(bands.hihat.value * 100));
     //socket.emit('color', 'hsl(' + Math.round(hue) + ', 100%, ' + Math.round(((intensity - minVal) / (imaxVal - minVal)) * 50) + '%)');
-    socket.emit('color', 'hsl(' + Math.round(hue) + ', 100%, ' + Math.round(ival * 1000) / 1000 + '%)');
+    //var colorBass = onecolor.color('hsl(' + Math.round(hue)
+    var vuColor = one('#000')
+        .red(bands.bass.value)
+        .blue(bands.snare.value)
+        //.green(bands.hihat.value)
+        .hue(hue, true);
+    socket.emit('color', vuColor.css());
 };
 
 var runFFT = function(newData) {
@@ -125,14 +109,9 @@ var runFFT = function(newData) {
             return 20 * Math.log10(band);
         });
         */
-        /*
-        _.each(output, function(band, index) {
-            avg[index] = avgFactor * avg[index] + (1 - avgFactor) * band;
-        });
-        */
 
-        avgResult(output);
-        printSpectrum(avg);
+        //avgResult(output);
+        printSpectrum(output);
     });
 };
 
